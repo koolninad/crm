b6cfb25ba1678fb02c82add36409c52d8ed6031e
Yash Wadgaonkar <wadgaonkaryash782@gmail.com>
2024-07-26 18:44:00 +0530

    minor changes related to smart flo integration

--- crm/fcrm/doctype/crm_lead/crm_lead.js
+++ crm/fcrm/doctype/crm_lead/crm_lead.js
@@ -1,8 +1,35 @@
-// Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on("CRM Lead", {
-	refresh(frm) {
-		frm.add_web_link(`/crm/leads/${frm.doc.name}`, __("Open in Portal"));
-	},
+    refresh(frm) { 
+        frm.add_web_link(`/crm/leads/${frm.doc.name}`, __("Open in Portal")); 
+        frm.add_custom_button(__('Call Lead'), function() {
+            let phone_number = frm.doc.mobile_no;
+
+            if (phone_number) {
+                frappe.call({
+                    method: 'smart_flo.api.fetch_agent.get_agent_info',
+                    args: {
+                        phone_number: phone_number
+                    },
+                    callback: function(response) {
+                        const result = response.message;
+                        if (result.message) {
+                            frappe.msgprint({
+                                title: __('Error'),
+                                message: result.message,
+                                indicator: 'red'
+                            });
+                        } else {
+                            frappe.msgprint({
+                                title: __('Success'),
+                                message: __('Call initiated successfully: ') + JSON.stringify(result),
+                                indicator: 'green'
+                            });
+                        }
+                    }
+                });
+            } else {
+                frappe.msgprint(__('Phone number is missing.'));
+            }
+        });
+    }
 });
